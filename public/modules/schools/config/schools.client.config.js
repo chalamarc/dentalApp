b03a1cc7c5bb8472875e4704fe587434b6f089a3
Chalama Challa <chalama@chalama.net>
2015-02-04 22:23:38 -0800

    Initial checkin

---
+++ public/modules/schools/config/schools.client.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('schools').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Schools', 'schools', 'dropdown', '/schools(/create)?');
+		Menus.addSubMenuItem('topbar', 'schools', 'List Schools', 'schools');
+		Menus.addSubMenuItem('topbar', 'schools', 'New School', 'schools/create');
+		Menus.addSubMenuItem('topbar', 'schools', 'Dashboard', 'schools/dashboard');
+	}
+]);
